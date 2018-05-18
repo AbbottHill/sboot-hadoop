58bacb7d52001bb7bba0bfe6f0baf78c1c1e6f60
AbbottHill <cdd9527@163.com>
2018-05-18 09:14:52 +0800

    hd

---
+++ src/main/java/com/cd/sboothd/SbootHdApplication.java
@@ -0,0 +1,12 @@
+package com.cd.sboothd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootHdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootHdApplication.class, args);
+	}
+}
