6659d6e76170f5e87432f3f3ec249e3c81339b1c
AbbottHill <cdd9527@163.com>
2018-05-28 17:27:23 +0800

    local submit job

--- src/main/java/com/cd/sboothd/mapred/WordCountDriver.java
+++ src/main/java/com/cd/sboothd/mapred/WordCountDriver.java
@@ -1,6 +1,8 @@
 package com.cd.sboothd.mapred;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -8,16 +10,21 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+@Log4j2
 public class WordCountDriver {
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         //1:指定为hdfs文件系统
         conf.set("fs.defaultFS", "hdfs://hdsm1:9000");
-        //2:指定jar包位置
-        conf.set("mapreduce.job.jar", "D:\\IdeaWorkSpace\\web-trunk\\out\\artifacts\\mrjob_jar\\mrjob.jar");
+
+        //2:指定jar包位置, 仅linux有效
+//        conf.set("mapreduce.job.jar", "D:\\IdeaWorkSpace\\web-trunk\\projects\\mrjob\\mrjob.jar");
+        conf.set("mapreduce.app-submission.cross-platform", "true");
+//        conf.set("mapred.jar", "D:\\IdeaWorkSpace\\web-trunk\\projects\\sboot-hd\\sboot-hd.jar");
 
         // 构造一个job对象来封装本mapreduce业务到所有信息
-        Job wcjob = Job.getInstance(conf);
+        Job wcjob = Job.getInstance(conf, "my Word Count");
 
         // 指定本job工作用到的jar包位置
         wcjob.setJarByClass(WordCountDriver.class);
@@ -36,10 +43,16 @@ public class WordCountDriver {
         wcjob.setOutputValueClass(LongWritable.class);
 
         // 指定程序处理到输入数据所在的路径
-        FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data/"));
-        // 指定程序处理到输出结果所在路径
-        FileOutputFormat.setOutputPath(wcjob, new Path("/wordcount/output1/"));
+        FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data"));
 
+        FileSystem fileSystem = FileSystem.get(conf);
+        Path path = new Path("/wordcount/output");
+        if (fileSystem.exists(path)) {
+            fileSystem.delete(path, true);
+        }
+
+        // 指定程序处理到输出结果所在路径
+        FileOutputFormat.setOutputPath(wcjob, path);
         // 将该job通过yarn的客户端进行提交
         wcjob.waitForCompletion(true);
     }
