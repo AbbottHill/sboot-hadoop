2013d1541ad5ceead78ccb81793879e2cad4d7c3
AbbottHill <cdd9527@163.com>
2018-06-23 17:37:27 +0800

    wordcount

--- src/main/java/com/cd/sbootspk/Operators.java
+++ src/main/java/com/cd/sbootspk/Operators.java
@@ -11,6 +11,7 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.ArrayList;
@@ -20,14 +21,65 @@ import java.util.List;
 
 import static com.cd.sbootspk.Operators.sc;
 
+/**
+ * genericity is important
+ */
 public class Operators {
     public static SparkConf conf = new SparkConf().
-            setAppName(MapOperator.class.getSimpleName())
+            setAppName(Operators.class.getSimpleName())
             .setMaster("local");
     public static JavaSparkContext sc = new JavaSparkContext(conf);
 }
 
-class MapOperator {
+/**
+ * todo
+ * aggregateByKey函数对PairRDD中相同Key的值进行聚合操作，在聚合过程中同样使用了一个中立的初始值。和aggregate函数类似，aggregateByKey返回值得类型不需要和RDD中value的类型一致。因为aggregateByKey是对相同Key中的值进行聚合操作，所以aggregateByKey函数最终返回的类型还是Pair RDD，对应的结果是Key和聚合好的值；而aggregate函数直接返回非RDD的结果。
+ *
+ * zeroValue：表示在每个分区中第一次拿到key值时,用于创建一个返回类型的函数,这个函数最终会被包装成先生成一个返回类型,然后通过调用seqOp函数,把第一个key对应的value添加到这个类型U的变量中。
+ seqOp：这个用于把迭代分区中key对应的值添加到zeroValue创建的U类型实例中。
+ combOp：这个用于合并每个分区中聚合过来的两个U类型的值。
+ */
+class AggregateByKey {
+    public static void main(String[] args) {
+
+        List<Tuple2<Integer, Integer>> datas = new ArrayList<>();
+        datas.add(new Tuple2<>(1, 3));
+        datas.add(new Tuple2<>(1, 2));
+        datas.add(new Tuple2<>(1, 4));
+        datas.add(new Tuple2<>(2, 3));
+
+        sc.parallelizePairs(datas, 2)
+                .aggregateByKey(
+                        0,
+                        new Function2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer v1, Integer v2) throws Exception {
+                                System.out.println("seq: " + v1 + "\t" + v2);
+                                return Math.max(v1, v2);
+                            }
+                        },
+                        new Function2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer v1, Integer v2) throws Exception {
+                                System.out.println("comb: " + v1 + "\t" + v2);
+                                return v1 + v2;
+                            }
+                        })
+                .collect()
+                .forEach(System.out::println);
+
+
+//        seq: 0	3
+//        seq: 3	2
+//        seq: 0	4
+//        seq: 0	3
+//        comb: 3	4
+//        (2,3)
+//        (1,7)
+    }
+}
+
+class Map_Operator {
 
     public static void main(String[] args) {
         List<String> datas = Arrays.asList(
@@ -101,9 +153,9 @@ class MapOperator {
 
 }
 
-class FilterOperator {
+class Filter_Operator {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setMaster("local").setAppName(FilterOperator.class.getSimpleName());
+        SparkConf conf = new SparkConf().setMaster("local").setAppName(Filter_Operator.class.getSimpleName());
         JavaSparkContext sc = new JavaSparkContext(conf);
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         JavaRDD<Integer> javaRDD = sc.parallelize(list);
@@ -113,9 +165,9 @@ class FilterOperator {
     }
 }
 
-class FlatMapOperator {
+class FlatMap_Operator {
     public static void main(String[] args) {
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName(FlatMapOperator.class.getSimpleName());
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName(FlatMap_Operator.class.getSimpleName());
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
         List<String> datas = Arrays.asList("a,1", "b,2", "c,3");
         JavaRDD<String> javaRDD = sc.parallelize(datas);
@@ -126,9 +178,9 @@ class FlatMapOperator {
 }
 
 
-class MapPartitionsOperator {
+class MapPartitions_Operator {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName(MapPartitionsOperator.class.getSimpleName())
+        SparkConf conf = new SparkConf().setAppName(MapPartitions_Operator.class.getSimpleName())
                 .setMaster("local");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -159,10 +211,10 @@ class MapPartitionsOperator {
     }
 }
 
-class MapPartitionsOperatorWithIndex {
+class MapPartitionsWithIndex_Operator {
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName(MapPartitionsOperator.class.getSimpleName())
+        SparkConf conf = new SparkConf().setAppName(MapPartitionsWithIndex_Operator.class.getSimpleName())
                 .setMaster("local");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -193,7 +245,7 @@ class MapPartitionsOperatorWithIndex {
     }
 }
 
-class GroupByKeyOperator {
+class GroupByKey_Operator {
     public static void main(String[] args) {
         List<Tuple2<Object, Object>> list = Arrays.asList(new Tuple2<Object, Object>('a', 1),
                 new Tuple2<Object, Object>('b', 2),
@@ -202,7 +254,7 @@ class GroupByKeyOperator {
         JavaPairRDD<Object, Object> pairRDD = sc.parallelizePairs(list);
         pairRDD.foreach(v -> System.out.println(v._1 + ": " + v._2));
 
-        pairRDD.groupByKey().foreach((VoidFunction<Tuple2<Object, Iterable<Object>>>) tuple -> {
+        pairRDD.groupByKey().foreach(tuple -> {
             StringBuilder stringBuilder = new StringBuilder(tuple._1.toString()).append("-> ");
             Iterator<Object> iterator = tuple._2.iterator();
             while (iterator.hasNext()) {
@@ -214,13 +266,191 @@ class GroupByKeyOperator {
     }
 }
 
-class DistinctOperator {
+class Distinct_Operator {
     public static void main(String[] args) {
+        SparkSession ss = SparkSession.builder().appName(Distinct_Operator.class.getSimpleName()).master("local").getOrCreate();
+        JavaSparkContext sc = new JavaSparkContext(ss.sparkContext());
+        List<Tuple2<String, Integer>> list = Arrays.asList(
+                new Tuple2<>("a", 1),
+                new Tuple2<>("a", 4),
+                new Tuple2<>("a", 1),
+                new Tuple2<>("b", 1),
+                new Tuple2<>("c", 1)
+        );
+        JavaPairRDD<String, Integer> pairRDD = sc.parallelizePairs(list);
+        // distinct
+        JavaPairRDD<String, Integer> distinct = pairRDD.distinct();
+        distinct.foreach(s -> System.out.println(s));
+
+        // distinct operator customized
+        JavaPairRDD<Tuple2<String, Integer>, Object> tuple2JavaPairRDD = pairRDD.mapToPair(tuple -> new Tuple2<>(new Tuple2<>(tuple._1, tuple._2), null));
+        tuple2JavaPairRDD.groupByKey().foreach(t -> System.out.println(t._1));
+
         sc.close();
     }
 }
 
+/**
+ * 对RDD进行抽样，其中参数withReplacement为true时表示抽样之后还放回，可以被多次抽样，false表示不放回；
+ * fraction表示抽样比例；seed为随机数种子，比如当前时间戳
+ */
+class Sample_Operator {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().appName("sample operator").master("local").getOrCreate();
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6);
+        JavaRDD<Integer> parallelize = jsc.parallelize(ints);
+        JavaRDD<Integer> sample = parallelize.sample(false, 0.8, System.currentTimeMillis());
+        sample.foreach(i -> System.out.println(i));
+    }
+}
+
+/**
+ * 合并两个RDD，不去重，要求两个RDD中的元素类型一致
+ */
+class Union_Operator {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName(Union_Operator.class.getSimpleName())
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<String> datas1 = Arrays.asList("张三", "李四");
+        List<String> datas2 = Arrays.asList("tom", "gim");
+
+        JavaRDD<String> data1RDD = sc.parallelize(datas1);
+        JavaRDD<String> data2RDD = sc.parallelize(datas2);
+
+        JavaRDD<String> unionRDD = data1RDD
+                .union(data2RDD);
+
+        unionRDD.foreach(v -> System.out.println(v));
+
+        sc.close();
+    }
+}
+
+/**
+ *返回两个RDD的交集
+ */
+class Intersection_Operator {
+    public static void main(String[] args) {
+        List<String> datas1 = Arrays.asList("张三", "李四", "tom");
+        List<String> datas2 = Arrays.asList("tom", "gim");
+
+        sc.parallelize(datas1)
+                .intersection(sc.parallelize(datas2))
+                .foreach(v -> System.out.println(v));
+    }
+}
+
+class Join_Operations {
+    public static void main(String[] args) {
+        List<Tuple2<Integer, String>> products = new ArrayList<>();
+        products.add(new Tuple2<>(1, "苹果"));
+        products.add(new Tuple2<>(2, "梨"));
+        products.add(new Tuple2<>(3, "香蕉"));
+        products.add(new Tuple2<>(4, "石榴"));
+
+        List<Tuple2<Integer, Integer>> counts = new ArrayList<>();
+        counts.add(new Tuple2<>(1, 7));
+        counts.add(new Tuple2<>(2, 3));
+        counts.add(new Tuple2<>(3, 8));
+        counts.add(new Tuple2<>(4, 3));
+        counts.add(new Tuple2<>(5, 9));
+
+        JavaPairRDD<Integer, String> productsRDD = sc.parallelizePairs(products);
+        JavaPairRDD<Integer, Integer> countsRDD = sc.parallelizePairs(counts);
+
+        productsRDD.join(countsRDD)
+                .foreach(v -> System.out.println(v));
+    }
+}
+
+/**
+ * cogroup 扩分区
+ */
+class Cogroup_Operators {
+    public static void main(String[] args) {
+        List<Tuple2<Integer, String>> datas1 = new ArrayList<>();
+        datas1.add(new Tuple2<>(1, "苹果"));
+        datas1.add(new Tuple2<>(2, "梨"));
+        datas1.add(new Tuple2<>(3, "香蕉"));
+        datas1.add(new Tuple2<>(4, "石榴"));
+
+        List<Tuple2<Integer, Integer>> datas2 = new ArrayList<>();
+        datas2.add(new Tuple2<>(1, 7));
+        datas2.add(new Tuple2<>(2, 3));
+        datas2.add(new Tuple2<>(3, 8));
+        datas2.add(new Tuple2<>(4, 3));
 
 
+        List<Tuple2<Integer, String>> datas3 = new ArrayList<>();
+        datas3.add(new Tuple2<>(1, "7"));
+        datas3.add(new Tuple2<>(2, "3"));
+        datas3.add(new Tuple2<>(3, "8"));
+        datas3.add(new Tuple2<>(4, "3"));
+        datas3.add(new Tuple2<>(4, "4"));
+        datas3.add(new Tuple2<>(4, "5"));
+        datas3.add(new Tuple2<>(4, "6"));
+
+        sc.parallelizePairs(datas1)
+                .cogroup(sc.parallelizePairs(datas2),
+                        sc.parallelizePairs(datas3))
+                .foreach(v -> System.out.println(v));
+    }
+}
+
+class SortByKey {
+    public static void main(String[] args) {
+        List<Integer> datas = Arrays.asList(60, 70, 80, 55, 45, 75);
+
+//    sc.parallelize(datas)
+//            .sortBy(new Function<Integer, Object>() {
+//                @Override
+//                public Object call(Integer v1) throws Exception {
+//                    return v1;
+//                }
+//            }, true, 1)
+//            .foreach(v -> System.out.println(v));
+
+        sc.parallelize(datas)
+                .sortBy((Integer v1) -> v1, false, 1)
+                .foreach(v -> System.out.println(v));
+
+        List<Tuple2<Integer, Integer>> datas2 = new ArrayList<>();
+        datas2.add(new Tuple2<>(3, 3));
+        datas2.add(new Tuple2<>(2, 2));
+        datas2.add(new Tuple2<>(1, 4));
+        datas2.add(new Tuple2<>(2, 3));
+
+        sc.parallelizePairs(datas2)
+                .sortByKey(false)
+                .foreach(v -> System.out.println(v));
+    }
+}
+
+/**
+ * Cartesian product
+ */
+class Cartesian {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("张三", "李四", "王五");
+        List<Integer> scores = Arrays.asList(60, 70, 80);
+
+        JavaRDD<String> namesRDD = sc.parallelize(names);
+        JavaRDD<Integer> scoreRDD = sc.parallelize(scores);
+
+        JavaPairRDD<String, Integer> cartesianRDD = namesRDD.cartesian(scoreRDD);
+        cartesianRDD.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+
+            private static final long serialVersionUID = 1L;
+
+            public void call(Tuple2<String, Integer> t) throws Exception {
+                System.out.println(t._1 + "\t" + t._2());
+            }
+        });
+    }
+}
 
     
--- src/main/java/com/cd/sbootspk/WordCount.java
+++ src/main/java/com/cd/sbootspk/WordCount.java
@@ -1,51 +1,27 @@
 package com.cd.sbootspk;
 
-import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class WordCount {
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("WordCount").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-
-        JavaRDD<String> lines = sc.textFile("projects/sboot-hd/datas/wc.txt");
-        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterator<String> call(String s) throws Exception {
-                return Arrays.asList(s.split(" ")).iterator();
-            }
-        });
-
-        JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String word) throws Exception {
-                return new Tuple2<String, Integer>(word, 1);
-            }
-        });
-
-        JavaPairRDD<String, Integer> wordcount = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer v1, Integer v2) throws Exception {
-                return v1 + v2;
-            }
-        });
-
-        for (Tuple2<String, Integer> pair : wordcount.collect()) {
-            System.out.println(pair._1 + ": " + pair._2);
-        }
-
-        sc.close();
+        SparkSession spark = SparkSession.builder().appName(WordCount.class.getSimpleName()).master("local").getOrCreate();
+        SparkContext sc = spark.sparkContext();
+        JavaRDD<String> lines = sc.textFile("projects/sboot-hd/datas/wc.txt", 1).toJavaRDD();
+//        JavaRDD<String> lines = spark.read().textFile("projects/sboot-hd/datas/wc.txt").javaRDD();
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
+        JavaPairRDD<String, Integer> pairRDD = words.mapToPair(w -> new Tuple2<>(w, 1));
+        JavaPairRDD<String, Integer> wordCount = pairRDD.reduceByKey((c1, c2) -> c1 + c2);
+        JavaPairRDD<Integer, String> mapToPair = wordCount.mapToPair(t -> new Tuple2<>(t._2, t._1));
+        JavaPairRDD<Integer, String> sortedPair = mapToPair.sortByKey(false);
+        JavaPairRDD<String, Integer> result = sortedPair.mapToPair(t -> new Tuple2<>(t._2, t._1));
+        result.foreach(s -> System.out.println(s));
     }
-
 }
     
